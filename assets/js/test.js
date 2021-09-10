c0d637bf280ddbc8026785405abd05967196d3bc
Kartiki Saraf <kartikisaraf@yahoo.com>
2021-09-10 13:00:09 -0700

    initial code to validate api calls return data

---
+++ assets/js/test.js
@@ -0,0 +1,59 @@
+
+// https://rapidapi.com/digiwalls/api/free-to-play-games-database/
+
+
+// console.log("Inside test.js")
+
+// ///////////////////////////////////////////////////
+// Filter games by multiple tags
+// works - returned 76 records
+// fetch("https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+// 		"x-rapidapi-key": "0f14760277msh4633a73019285dap13a775jsnde37e09289b4"
+// 	}
+// })
+// .then(response => {
+//     if (response.ok) {
+//         response.json().then(function(data) {           
+//             console.log(data);           
+//         });
+//     } else {
+//         alert(`Error: ${response.statusText}`)
+//     }
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+// //////////////////////////////////////////////////////////////
+
+
+// ///////////////////////////////////
+// return details from a specific game
+// works
+// fetch("https://free-to-play-games-database.p.rapidapi.com/api/game?id=452", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+// 		"x-rapidapi-key": "0f14760277msh4633a73019285dap13a775jsnde37e09289b4"
+// 	}
+// })
+// .then(response => {
+    // if (response.ok) {
+    //     response.json().then(function(data) {
+           
+    //         console.log(data);
+           
+    //     });
+    // } else {
+    //     alert(`Error: ${response.statusText}`)
+    // }
+// 	console.log(response);
+    
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+// // ///////////////////////////////////
