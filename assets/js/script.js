d0bd417f923d25b27088ec1973aa79038d30cefe
Carlos <carlos96831@gmail.com>
2021-09-17 01:14:40 -0700

    changed script

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,25 @@
 // 404: Object not found: Game or endpoint not found
 // 500: Something wrong on our end (unexpected server errors)
 
-// returns 1 record
+// Get games list - returns 365 records
+// 365 items
+//     [100 items
+//         0:{11 items
+//             "id":1
+//             "title":"Dauntless"
+//             "thumbnail":"https://www.freetogame.com/g/1/thumbnail.jpg"
+//             "short_description":"A free-to-play, co-op action RPG with gameplay similar to Monster Hunter."
+//             "game_url":"https://www.freetogame.com/open/dauntless"
+//             "genre":"MMORPG"
+//             "platform":"PC (Windows)"
+//             "publisher":"Phoenix Labs"
+//             "developer":"Phoenix Labs, Iron Galaxy"
+//             "release_date":"2019-05-21"
+//             "freetogame_profile_url":"https://www.freetogame.com/dauntless"
+//             }
+
+
+// Get details for a specific game - returns 1 record// 
 // {15 items
 //     "id":452
 //     "title":"Call Of Duty: Warzone"
@@ -54,6 +72,8 @@
 //     "freetogame_profile_url":"https://www.freetogame.com/jade-goddess"
 //     }
 
+var favoriteArr = [];
+
 var getGamesList = function() {
     // var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -73,12 +93,138 @@ var getGamesList = function() {
     .then(response => {
         if (response.ok) {
             response.json().then(function(data) {             
-                console.log(data);               
+                console.log(data);          
+                // console.log(data[0]);   
+
+                for(i=0; i<7; i++)
+                {
+                    var cardTitleEl = document.querySelector(`#card${i+1}-title`);
+                    cardTitleEl.textContent = data[i].title;
+                    // console.log("cardTitleEl.textContent: " + cardTitleEl.textContent); 
+
+                    var cardShortDescriptionEl = document.querySelector(`#card${i+1}-short-description`);
+                    cardShortDescriptionEl.textContent = data[i].short_description;
+                    // console.log("card1-short-description: " + cardShortDescriptionEl.textContent); 
+
+                    var cardImageEl = document.querySelector(`#card${i+1}-img`);
+                    cardImageEl.src = data[i].thumbnail;
+                    // console.log(cardImageEl); 
+
+                }
+                // // Card 1
+                // var card1TitleEl = document.querySelector('#card1-title');
+                // card1TitleEl.textContent = data[0].title;
+                // // console.log("card1TitleEl.textContent: " + card1TitleEl.textContent); 
+
+                // var card1ShortDescriptionEl = document.querySelector('#card1-short-description');
+                // card1ShortDescriptionEl.textContent = data[0].short_description;
+                // // console.log("card1-short-description: " + card1ShortDescriptionEl.textContent); 
+
+                // var card1ImageEl = document.querySelector('#card1-img');
+                // card1ImageEl.src = data[0].thumbnail;
+                // // console.log(card1ImageEl.src); 
+
+                // // Card 2
+                // var card2TitleEl = document.querySelector('#card2-title');
+                // card2TitleEl.textContent = data[1].title;
+                // // console.log("card2TitleEl.textContent: " + card2TitleEl.textContent); 
+
+                // var card2ShortDescriptionEl = document.querySelector('#card2-short-description');
+                // card2ShortDescriptionEl.textContent = data[1].short_description;
+                // // console.log("card2-short-description: " + card2ShortDescriptionEl.textContent); 
+
+                // var card2ImageEl = document.querySelector('#card2-img');
+                // card2ImageEl.src = data[1].thumbnail;
+                // // console.log(card2ImageEl.src); 
+
+                // // Card 3
+                // var card3TitleEl = document.querySelector('#card3-title');
+                // card3TitleEl.textContent = data[2].title;
+                // // console.log("card3TitleEl.textContent: " + card3TitleEl.textContent); 
+
+                // var card3ShortDescriptionEl = document.querySelector('#card3-short-description');
+                // card3ShortDescriptionEl.textContent = data[2].short_description;
+                // // console.log("card3-short-description: " + card3ShortDescriptionEl.textContent); 
+
+                // var card3ImageEl = document.querySelector('#card3-img');
+                // card3ImageEl.src = data[2].thumbnail;
+                // // console.log(card3ImageEl.src); 
+
+                // // Card 4
+                // var card4TitleEl = document.querySelector('#card4-title');
+                // card4TitleEl.textContent = data[3].title;
+                // // console.log("card4TitleEl.textContent: " + card4TitleEl.textContent); 
+
+                // var card4ShortDescriptionEl = document.querySelector('#card4-short-description');
+                // card4ShortDescriptionEl.textContent = data[3].short_description;
+                // // console.log("card4-short-description: " + card4ShortDescriptionEl.textContent); 
+
+                // var card4ImageEl = document.querySelector('#card4-img');
+                // card4ImageEl.src = data[3].thumbnail;
+                // // console.log(card4ImageEl.src); 
+
+                // // Card 5
+                // var card5TitleEl = document.querySelector('#card5-title');
+                // card5TitleEl.textContent = data[4].title;
+                // // console.log("card5TitleEl.textContent: " + card5TitleEl.textContent); 
+
+                // var card5ShortDescriptionEl = document.querySelector('#card5-short-description');
+                // card5ShortDescriptionEl.textContent = data[4].short_description;
+                // // console.log("card5-short-description: " + card5ShortDescriptionEl.textContent); 
+
+                // var card5ImageEl = document.querySelector('#card5-img');
+                // card5ImageEl.src = data[4].thumbnail;
+                // // console.log(card5ImageEl.src); 
+
+                // // Card 6
+                // var card6TitleEl = document.querySelector('#card6-title');
+                // card6TitleEl.textContent = data[5].title;
+                // // console.log("card6TitleEl.textContent: " + card6TitleEl.textContent); 
+
+                // var card6ShortDescriptionEl = document.querySelector('#card6-short-description');
+                // card6ShortDescriptionEl.textContent = data[5].short_description;
+                // // console.log("card6-short-description: " + card6ShortDescriptionEl.textContent); 
+
+                // var card6ImageEl = document.querySelector('#card6-img');
+                // card5ImageEl.src = data[5].thumbnail;
+                // // console.log(card6ImageEl.src); 
+
+
+                // // Card 7
+                // var card7TitleEl = document.querySelector('#card7-title');
+                // card7TitleEl.textContent = data[6].title;
+                // // console.log("card7TitleEl.textContent: " + card7TitleEl.textContent); 
+
+                // var card7ShortDescriptionEl = document.querySelector('#card7-short-description');
+                // card7ShortDescriptionEl.textContent = data[6].short_description;
+                // // console.log("card7-short-description: " + card7ShortDescriptionEl.textContent); 
+
+                // var card7ImageEl = document.querySelector('#card7-img');
+                // card7ImageEl.src = data[6].thumbnail;
+                // // console.log(card7ImageEl.src); 
+
+
+
+
+                // for(i = 0; i < data.length; i++)
+                // {
+                //     console.log("Id: " + data[i].id);
+                //     console.log("title: " + data[i].title);
+                //     console.log("thumbnail: " + data[i].thumbnail);                   
+                //     console.log("short_description: " + data[i].short_description);                    
+                //     console.log("game_url: " + data[i].game_url);
+                //     console.log("genre: " + data[i].genre);
+                //     console.log("platform: " + data[i].platform);
+                //     console.log("publisher: " + data[i].publisher);
+                //     console.log("developer: " + data[i].developer);
+                //     console.log("release_date: " + data[i].release_date);
+                //     console.log("freetogame_profile_url: " + data[i].freetogame_profile_url);
+                // }
             });
         } else {
             alert(`Error: ${response.statusText}`)
         }
-        console.log(response);
+        // console.log(response);
     })
     .catch(err => {
         console.error(err);
@@ -146,9 +292,7 @@ var getGamesFilter = function() {
     })
     .then(response => {
         if (response.ok) {
-            response.json().then(function(data) {
-             
-                
+            response.json().then(function(data) {                
                 
                 console.log("Response is OK");
                 console.log(data);
@@ -250,6 +394,190 @@ var getGamesFilter = function() {
     });
 }
 
-btnGetGamesList.addEventListener('click', getGamesList);
+// save favorite into local storage
+var saveFavoriteCard = function(cardId) {
+    var c1 = document.querySelector(`#${cardId}-title`);
+    var favorite = c1.textContent;
+    loadFavorites();
+    // prevent duplicate favorite from being saved and move it to end of array
+    for (var i = 0; i < favoriteArr.length; i++) {
+        if (favorite === favoriteArr[i]) {
+            favoriteArr.splice(i, 1);
+        }
+    }
+    favoriteArr.push(favorite);
+    localStorage.setItem('myFavoriteGames', JSON.stringify(favoriteArr));
+}
+
+// save favorite into local storage
+// var saveFavoriteCard2 = function() {
+//     var c1 = document.querySelector('#card2-title');
+//     var favorite = c1.textContent;
+//     loadFavorites();
+//     // prevent duplicate favorite from being saved and move it to end of array
+//     for (var i = 0; i < favoriteArr.length; i++) {
+//         if (favorite === favoriteArr[i]) {
+//             favoriteArr.splice(i, 1);
+//         }
+//     }
+//     favoriteArr.push(favorite);
+//     localStorage.setItem('favorites', JSON.stringify(favoriteArr));
+// }
+
+// load favorites from local storage
+var loadFavorites = function() {
+    favoriteArr = JSON.parse(localStorage.getItem('myFavoriteGames'));
+
+    console.log(favoriteArr);
+
+    if (!favoriteArr) {
+        favoriteArr = [];
+        return false;
+    } 
+    // else if (favoriteArr.length > 5) {
+    //     // save only the five most recent favorites
+    //     favoriteArr.shift();
+    // }
+    
+    for (var i = 0; i < favoriteArr.length; i++) {
+        var g1 = document.querySelector(`#game${i+1}`);
+        g1.textContent = favoriteArr[i]; 
+        g1.href = "pokemon.html"; //TODO: add url of the game        
+    }
+
+      // set recent cities
+    // var recentCities = document.querySelector('#fDiv');
+    // var cityListUl = document.createElement('ul');
+    // cityListUl.className = 'list-group list-group-flush city-list';
+    // recentCities.appendChild(cityListUl);
+
+    // for (var i = 0; i < favoriteArr.length; i++) {
+    //     var cityListItem = document.createElement('button');
+    //     cityListItem.setAttribute('type', 'button');
+    //     cityListItem.className = 'list-group-item';
+    //     cityListItem.setAttribute('value', favoriteArr[i]);
+    //     cityListItem.textContent = favoriteArr[i];
+    //     cityListUl.prepend(cityListItem);
+    // }
+
+    // var cityList = document.querySelector('.city-list');
+    // cityList.addEventListener('click', selectRecent)
+
+
+    // https://www.geeksforgeeks.org/how-to-create-a-link-in-javascript/
+    // var myFavorites = document.querySelector('#favoriteList');
+    // // var a = document.querySelector('#game1');
+    // var a = document.createElement('a'); 
+    // a.className = "collection-item";
+    // var link = document.createTextNode("This is link");
+    // a.appendChild(link);
+    // a.title = "This is link";
+    // a.href = "https://www.geeksforgeeks.org";
+    // myFavorites.appendChild(a);
+
+    // var myFavorites = document.querySelector('#favoriteList');
+    // var favoriteListA = document.createElement('a');
+    // var link = document.createTextNode("This is link");
+    // a.appendChild(link); 
+    // a.title = "This is Link";
+    // a.href = "https://www.geeksforgeeks.org"; 
+
+    // // favoriteListA.className = 'collection-item';
+    // // favoriteListA.setAttribute('value', "test");
+    // myFavorites.appendChild(favoriteListA);
+
+    // for (var i = 0; i < favoriteArr.length; i++) {
+    //     var cityListItem = document.createElement('button');
+    //     cityListItem.setAttribute('type', 'button');
+    //     cityListItem.className = 'list-group-item';
+    //     cityListItem.setAttribute('value', cityArr[i]);
+    //     cityListItem.textContent = cityArr[i];
+    //     cityListUl.prepend(cityListItem);
+    // }
+
+    // set recent cities
+    // var recentCities = document.querySelector('#recent-cities');
+    // var cityListUl = document.createElement('ul');
+    // cityListUl.className = 'list-group list-group-flush city-list';
+    // recentCities.appendChild(cityListUl);
+
+    // for (var i = 0; i < cityArr.length; i++) {
+    //     var cityListItem = document.createElement('button');
+    //     cityListItem.setAttribute('type', 'button');
+    //     cityListItem.className = 'list-group-item';
+    //     cityListItem.setAttribute('value', cityArr[i]);
+    //     cityListItem.textContent = cityArr[i];
+    //     cityListUl.prepend(cityListItem);
+    // }
+
+    // var cityList = document.querySelector('.city-list');
+    // cityList.addEventListener('click', selectRecent)
+}
+
+
+getGamesList();
+
+
+// btnGetGamesList.addEventListener('click', getGamesList);
 
 btnGetGamesFilter.addEventListener('click', getGamesFilter);
+
+// initialize modal - https://materializecss.com/modals.html
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+
+    // M.AutoInit();
+  });
+
+
+  
+
+// card1-add.addEventListener('click', saveFavorite);
+//   document.addEventListener('onclick', function() {
+    // var elem = document.querySelectorAll('.fixed-action-btn');    
+    // var instances = M.FloatingActionButton.init(elems, options);    
+//   });
+
+// function test(data){
+//     console.log(data);
+// }
+
+//   var card1Btn = document.querySelector('#card1-add');
+//   card1Btn.addEventListener('click', () => {
+//       saveFavoriteCard()
+    
+//     });
+
+
+
+//   var card2Btn = document.querySelector('#card2-add');
+//   card2Btn.addEventListener('click', saveFavoriteCard2);
+
+//   var card3Btn = document.querySelector('#card3-add');
+//   card3Btn.addEventListener('click', saveFavoriteCard3);
+
+//   var card4Btn = document.querySelector('#card4-add');
+//   card4Btn.addEventListener('click', saveFavoriteCard4);
+
+//   var card5Btn = document.querySelector('#card5-add');
+//   card5Btn.addEventListener('click', saveFavoriteCard5);
+
+//   var card6Btn = document.querySelector('#card6-add');
+//   card6Btn.addEventListener('click', saveFavoriteCard6);
+
+//   var card7Btn = document.querySelector('#card7-add');
+//   card7Btn.addEventListener('click', saveFavoriteCard7);
+
+
+//   https://materializecss.com/floating-action-button.html
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.fixed-action-btn');
+//     var instances = M.FloatingActionButton.init(elems, options);
+//   });
+
+//   var instance = M.FloatingActionButton.getInstance(elem);
+//   instance.open();
+
+//   instance.close();
+//   instance.destroy();
